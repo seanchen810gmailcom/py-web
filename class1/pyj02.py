ec4e6a6af857a95650a90ad8572fdabb0c0bff74
seanchen810gmailcom <seanchen810@gmail.com>
2026-03-08 11:01:38 +0800

    新增 GUI 應用程式，包含開啟 Safari 瀏覽器的按鈕及顯示/清除標籤的功能

--- class1/pyj02.py
+++ class1/pyj02.py
@@ -9,20 +9,30 @@ def open_safari():
     os.system("open -a Safari")  # 使用os.system()方法打開Safari瀏覽器
 
 
+def open_function():
+    label.config(text="Hello, World!", bg="black", fg="red")  # 更新標籤的文字內容
+
+
+def close_function():
+    label.config(text="", bg=windows.cget("bg"))  # 清空標籤的文字內容，清掉背景顏色
+
+
 #######################建立視窗########################
 # 創建視窗
 windows = Tk()
 # 設定主視窗標題
 windows.title("My First GUI")
 ########################建立按鈕#####################
-button = Button(
-    windows, text="Open Safari", command=open_safari
+btn1 = Button(
+    windows, text="Show Screen", command=open_function
 )  # 創建一個按鈕，點擊後執行open_safari函數
-button.pack()  # 使用pack()方法將按鈕放在視窗中
+btn1.pack()  # 使用pack()方法將按鈕放在視窗中
+btn2 = Button(
+    windows, text="Close Screen", command=close_function
+)  # 創建一個按鈕，點擊後執行open_safari函數
+btn2.pack()  # 使用pack()方法將按鈕放在視窗中
 ##########################建立標籤#####################
-label = Label(
-    windows, text="Hello, World!", fg="red", bg="black"
-)  # 創建一個標籤，顯示文字
+label = Label(windows, text="")  # 創建一個標籤，顯示文字
 label.pack()  # 使用pack()方法將標籤放在視窗中
 #######################運行應用程式#####################
 # 開始執行主回圈，等待用戶操作
