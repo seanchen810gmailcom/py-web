d77ff7991eb22fe9acf70c377b638a0ec5c23f90
seanchen810gmailcom <seanchen810@gmail.com>
2026-03-15 11:03:45 +0800

    新增 GUI 應用程式，包含隨機顏色標籤及清空標籤功能

--- class2/pyj01.py
+++ class2/pyj01.py
@@ -1,22 +1,20 @@
 #######################匯入模組#######################
 from tkinter import *
 import os
-
+import random
 
 # 匯入thinter模組，用*匯入所有內容
 #######################定義函數########################
-def open_safari():
-    os.system("open -a Safari")  # 使用os.system()方法打開Safari瀏覽器
 
 
 def open_function():
-    label.config(text="Hello, World!", bg="black", fg="red")  # 更新標籤的文字內容
-
-
-def close_function():
-    label.config(text="", bg=windows.cget("bg"))  # 清空標籤的文字內容，清掉背景顏色
-    # TK不支援透明背景，所以需要取下背景螢幕，當作背景，得到透明的效果
-    # 也可以用 SystemBottenFace
+    global aready_clicked
+    if aready_clicked == False:
+        label.config(text="Hello, World!", bg="red", fg="black")  # 更新標籤的文字內容
+        aready_clicked = True
+    else:
+        label.config(text="Hello, World!", bg="green", fg="black")  # 更新標籤的文字內容
+        aready_clicked = False
 
 
 #######################建立視窗########################
@@ -29,10 +27,6 @@ btn1 = Button(
     windows, text="Show Screen", command=open_function
 )  # 創建一個按鈕，點擊後執行open_safari函數
 btn1.pack()  # 使用pack()方法將按鈕放在視窗中
-btn2 = Button(
-    windows, text="Close Screen", command=close_function
-)  # 創建一個按鈕，點擊後執行open_safari函數
-btn2.pack()  # 使用pack()方法將按鈕放在視窗中
 ##########################建立標籤#####################
 label = Label(windows, text="")  # 創建一個標籤，顯示文字
 label.pack()  # 使用pack()方法將標籤放在視窗中
--- class2/pyj02.py
+++ class2/pyj02.py
@@ -1,22 +1,33 @@
 #######################匯入模組#######################
 from tkinter import *
 import os
-
+import random
 
 # 匯入thinter模組，用*匯入所有內容
 #######################定義函數########################
-def open_safari():
-    os.system("open -a Safari")  # 使用os.system()方法打開Safari瀏覽器
 
 
 def open_function():
-    label.config(text="Hello, World!", bg="black", fg="red")  # 更新標籤的文字內容
-
-
-def close_function():
-    label.config(text="", bg=windows.cget("bg"))  # 清空標籤的文字內容，清掉背景顏色
-    # TK不支援透明背景，所以需要取下背景螢幕，當作背景，得到透明的效果
-    # 也可以用 SystemBottenFace
+    fg_random_color = "#" + "".join(
+        [random.choice("0123456789ABCDEF") for i in range(6)]
+    )  # 隨機生成一個顏色的十六進制值
+    bg_random_color = "#" + "".join(
+        [random.choice("0123456789ABCDEF") for i in range(6)]
+    )  # 隨機生成一個顏色的十六進制值
+    label.config(
+        text="Hello, World!", bg=f"{bg_random_color}", fg=f"{fg_random_color}"
+    )  # 更新標籤的文字內容
+    """
+    比對展開寫法：
+    fg_random_color = "#"
+    for i in range(6):
+        fg_random_color += random.choice("0123456789abcdef")
+    bg_random_color = "#"
+    for i in range(6):
+        bg_random_color += random.choice("0123456789abcdef")
+    label.config(
+        text="Hello, World!", bg=f"{bg_random_color}", fg=f"{fg_random
+    """
 
 
 #######################建立視窗########################
@@ -24,15 +35,13 @@ def close_function():
 windows = Tk()
 # 設定主視窗標題
 windows.title("My First GUI")
+windows.geometry("400x300")  # 設定視窗大小
+windows.option_add("*Font", "行街行階 20")  # 設定全局字體
 ########################建立按鈕#####################
 btn1 = Button(
     windows, text="Show Screen", command=open_function
 )  # 創建一個按鈕，點擊後執行open_safari函數
 btn1.pack()  # 使用pack()方法將按鈕放在視窗中
-btn2 = Button(
-    windows, text="Close Screen", command=close_function
-)  # 創建一個按鈕，點擊後執行open_safari函數
-btn2.pack()  # 使用pack()方法將按鈕放在視窗中
 ##########################建立標籤#####################
 label = Label(windows, text="")  # 創建一個標籤，顯示文字
 label.pack()  # 使用pack()方法將標籤放在視窗中
